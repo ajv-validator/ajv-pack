8dba26e4098d1915cc6e317de555849d5d4b8d62
Evgeny Poberezkin <e.poberezkin@me.com>
2016-09-24 14:46:45 +0100

    test: AjvPack instance

---
+++ spec/instance.spec.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var Ajv = require('Ajv');
+var AjvPack = require('..').instance;
+var assert = require('assert');
+
+describe('AjvPack instance', function() {
+  var wrappedAjv;
+
+  beforeEach(function() {
+    wrappedAjv = new AjvPack(new Ajv);
+  });
+
+  it('should compile schemas', function() {
+    var schema = { type: 'string' };
+    var validate = wrappedAjv.compile(schema);
+    assert.strictEqual(validate('foo'), true);
+    assert.strictEqual(validate(1), false);
+  });
+
+  it('should validate data', function() {
+    var schema = { type: 'string' };
+    assert.strictEqual(wrappedAjv.validate(schema, 'foo'), true);
+    assert.strictEqual(wrappedAjv.validate(schema, 1), false);
+  });
+
+  it('should support addSchema', function() {
+    var strSchema = { id: 'str', type: 'string' };
+    wrappedAjv.addSchema(strSchema);
+    var schema = { $ref: 'str' };
+    assert.strictEqual(wrappedAjv.validate(schema, 'foo'), true);
+    assert.strictEqual(wrappedAjv.validate(schema, 1), false);
+  });
+});
